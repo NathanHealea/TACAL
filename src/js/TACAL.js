8c764b928f225be04e12f708f32eede4fffbc646
NathanHealea <nate.healea@gmail.com>
2016-03-15 09:46:30 -0700

    Update TACAL.js Added global variables. -- added days, -- added months -- added Number of calendar days

--- src/js/TACAL.js
+++ src/js/TACAL.js
@@ -11,3 +11,20 @@ var TACAL;
 /* Initializing TACAL object */
 TACAL = TACAL || {};
 
+/**
+ * Days of the week
+ * @type {string[]}
+ */
+TACAL.DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+/**
+ * Months of the year
+ * @type {string[]}
+ */
+TACAL.MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+/**
+ * Number of days in each month
+ * @type {number[]}
+ */
+TACAL.CALDAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
